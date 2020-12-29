e414d60c2e8303f9714e1133c5ad1437172a5864
tuan.truong@metanet.co.kr <tuan.truong@metanet.co.kr>
2020-12-29 18:14:43 +0700

    initial commit

---
+++ src/main/java/vn/microservice/streaming/payment/MicroserviceStreamingPaymentApplication.java
@@ -0,0 +1,13 @@
+package vn.microservice.streaming.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceStreamingPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceStreamingPaymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/microservice/streaming/payment/MicroserviceStreamingPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.microservice.streaming.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceStreamingPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
